e3526a139f1645555f1149c13ee7485058b0d216
liz-rondone <liz.rondone@northwestern.edu>
2017-05-22 20:47:04 -0500

    monday update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var targetNumber = "";
 var counter = 0;
 var won = 0;
 var loss = 0;
-var valueCrystal = "";	// data-crystalValue
+var valueCrystal;	// data-crystalValue
 var purpleCrystal, blueCrystal, whiteCrystal, greenCrystal;
 var crystalArray = [purpleCrystal, blueCrystal, whiteCrystal, greenCrystal];
 
@@ -16,8 +16,11 @@ function startGame() {	//no argument
 	greenCrystal = getRandomArbitrary(1,12);
 	valueCrystal = getRandomArbitrary(1,12);
 	counter = 0;
+	$(".image-responsive").click(valueCrystal);
 	$("#counter").text(counter);
 	$("#target-number").text(targetNumber);
+	$("#won-number").text(won);
+	$("#loss-number").text(loss);
 	console.log(purpleCrystal);
 	console.log(blueCrystal);
 	console.log(whiteCrystal);
@@ -32,20 +35,26 @@ startGame();
 
 
 $(".img-responsive").on("click", function() {
-	counter += 10; //replace with random number function
 
-	crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
+	// Determining the crystal's value requires us to extract the value from the data attribute.
+    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+	crystalArray = ($(this).attr("#data-valueCrystal"));
+    crystalArray = parseInt(crystalArray);
+    
+    // We then add the crystalArray to the user's "counter" which is a global variable.
     // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
+    counter += crystalArray;
 
     if (counter === targetNumber) {
-      document.querySelector('#endGameMessage').innerHTML = "Congratulations! You're a rockstar."
+    	document.querySelector('#endGameMessage').innerHTML = "Congratulations! You're a rockstar."
+    	won++;
     }
 
     else if (counter >= targetNumber) {
-      document.querySelector('#endGameMessage').innerHTML = "Whoops! You're just a rock."
+    	document.querySelector('#endGameMessage').innerHTML = "Whoops! You're just a rock."
+    	loss++;
     }
 
 });
