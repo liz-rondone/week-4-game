1a1891e71adfdc9d02c77cb3db8f9e3dbbc4adc7
liz-rondone <liz.rondone@northwestern.edu>
2017-05-24 19:35:20 -0500

    wednesday update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,25 +2,28 @@ var targetNumber = "";
 var counter = 0;
 var won = 0;
 var loss = 0;
-var valueCrystal;	// data-crystalValue
-var purpleCrystal, blueCrystal, whiteCrystal, greenCrystal;
-var crystalArray = [purpleCrystal, blueCrystal, whiteCrystal, greenCrystal];
+//var valueCrystal;	// data-crystalValue
+//var purpleCrystal, blueCrystal, whiteCrystal, greenCrystal;
+//var crystalArray = [purpleCrystal, blueCrystal, whiteCrystal, greenCrystal];
 
 
 
 function startGame() {	//no argument
 	targetNumber = getRandomArbitrary(19,120);
-	purpleCrystal = getRandomArbitrary(1,12);
-	blueCrystal = getRandomArbitrary(1,12);
-	whiteCrystal = getRandomArbitrary(1,12);
-	greenCrystal = getRandomArbitrary(1,12);
-	valueCrystal = getRandomArbitrary(1,12);
+	var purpleCrystal = getRandomArbitrary(1,12);
+	var blueCrystal = getRandomArbitrary(1,12);
+	var whiteCrystal = getRandomArbitrary(1,12);
+	var greenCrystal = getRandomArbitrary(1,12);
 	counter = 0;
-	$(".image-responsive").click(valueCrystal);
+	$(".image-responsive").click(crystalValue);
 	$("#counter").text(counter);
 	$("#target-number").text(targetNumber);
 	$("#won-number").text(won);
 	$("#loss-number").text(loss);
+	$("#purple").attr('data-crystalValue', purpleCrystal);
+	$("#blue").attr('data-crystalValue', blueCrystal);
+	$("#white").attr('data-crystalValue', whiteCrystal);
+	$("#green").attr('data-crystalValue', greenCrystal);
 	console.log(purpleCrystal);
 	console.log(blueCrystal);
 	console.log(whiteCrystal);
@@ -36,25 +39,28 @@ startGame();
 
 $(".img-responsive").on("click", function() {
 
+	// imageCrystal.attr("data-crystalvalue", numberOptions[i]);
 	// Determining the crystal's value requires us to extract the value from the data attribute.
     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
     // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-	crystalArray = ($(this).attr("#data-valueCrystal"));
-    crystalArray = parseInt(crystalArray);
+	var crystalClicked = ($(this).attr("#data-crystalValue"));
+    var crystalValue = parseInt(crystalClicked);
     
     // We then add the crystalArray to the user's "counter" which is a global variable.
     // Every click, from every crystal adds to the global counter.
-    counter += crystalArray;
+    counter += crystalClicked;
 
     if (counter === targetNumber) {
     	document.querySelector('#endGameMessage').innerHTML = "Congratulations! You're a rockstar."
     	won++;
+    	startGame();
     }
 
     else if (counter >= targetNumber) {
     	document.querySelector('#endGameMessage').innerHTML = "Whoops! You're just a rock."
     	loss++;
+    	startGame();
     }
 
 });
